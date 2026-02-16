c1e968619bbd8ed45c7d333e1a97e05fb2f3982f
Alphin.Varghese <Alphin.varghese@bridgeconn.com>
2026-02-16 13:41:42 +0530

    Recording feature using nitro sound

--- App.tsx
+++ App.tsx
@@ -1,45 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
+import { StatusBar, StyleSheet, useColorScheme, Text, View,Button, Platform, PermissionsAndroid } from 'react-native';
+import {SafeAreaProvider, useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import Sound from 'react-native-nitro-sound';
+import React,{useState} from 'react';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
+const[recording,setRecording] = useState(false);
+const [recordings, setRecordings] = useState<string[]>([]);
+const isDarkMode = useColorScheme() === 'dark';
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
+const requestPermission = async () => {
+if(Platform.OS==='android'){
+const granted=await PermissionsAndroid.request(
+PermissionsAndroid.PERMISSIONS.RECORD_AUDIO
+);
+return granted === PermissionsAndroid.RESULTS.GRANTED;
+}
+return true;
+};
+
+const startRecording = async () => {
+const ok = await requestPermission();
+if (!ok) return;
+const path = Platform.select({
+ios: 'recording.m4a',
+android: `${Date.now()}.mp4`
+});
+await Sound.startRecorder();
+setRecording(true);
+};
+const stopRecording = async () => {
+const uri = await Sound.stopRecorder();
+setRecording(false);
+
+setRecordings(prev => [...prev, uri]);
+};
+
+const playRecording = async (uri: string) => {
+await Sound.startPlayer(uri);
+};
+
+return (
+<View style={styles.container}>
+
+<Button
+title={recording ? "Stop Recording" : "Start Recording"}
+onPress={recording ? stopRecording : startRecording}
+/>
+
+{recordings.map((uri, index) => (
+<View key={index} style={styles.row}>
+<Text>Recording #{index + 1}</Text>
+<Button title="Play" onPress={() => playRecording(uri)} />
+</View>
+))}
+
+</View>
+);
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+container: {
+flex: 1,
+alignItems: 'center',
+justifyContent: 'center',
+},
 });
 
-export default App;
+
