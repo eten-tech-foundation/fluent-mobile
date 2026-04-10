ee57f1f4ef7afc9808f39e6f8b19494838e29418
Bernise.john <bernise.john@bridgeconn.com>
2026-04-10 12:15:47 +0530

    removed unwanted code

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,5 +3,5 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(@react-native|react-native|@react-navigation|react-native-safe-area-context|@react-native-vector-icons|react-native-screens|react-native-gesture-handler|react-native-svg)/)',
   ],
-  setupFiles: ['./jest.setup.js'],
+    testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
 };
