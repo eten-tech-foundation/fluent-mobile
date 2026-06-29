d5c2350ac5a2cd3f54ab7b18aee03fcb56d6602f
mattrace-gloo <mrace@gloo.us>
2026-06-29 12:41:38 -0700

    Fix StyleSheet.absoluteFill for Expo SDK 56 typecheck.

--- src/components/ui/CloudSyncStatusIcon.tsx
+++ src/components/ui/CloudSyncStatusIcon.tsx
@@ -354,7 +354,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   overlayFill: {
-    ...StyleSheet.absoluteFillObject,
+    ...StyleSheet.absoluteFill,
     alignItems: 'center',
     justifyContent: 'center',
   },
